1e52f081bc282b23d49486c724be89de6db6662e
Szilaay <86633348+Szilaay@users.noreply.github.com>
2024-10-07 12:14:30 +0200

    dolgozat készen
    Szücs Szilárd

---
+++ triatlon/triatlon/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace triatlon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzok> lista = new List<Versenyzok>();
+
+            StreamReader sr = new StreamReader("forras.txt");
+
+            while (!sr.EndOfStream)
+            {
+                Versenyzok b = new Versenyzok(sr.ReadLine());
+                lista.Add(b);
+            }
+
+            sr.Close();
+
+            Console.WriteLine($"Összesen ennyi versenyző fejezte be a versenyt > {lista.Count}");
+            Console.WriteLine($"Összesen ennyi versenyző van 'elit' kategóriában > {lista.Count(V => V.VEletkorKategoria == "elit")}");
+            Console.WriteLine($"Összesen ennyi volt a futással töltött idő > {lista.Sum(V => V.FutasIdo.TotalHours):0.00}");
+
+            Console.WriteLine($"Átlagos úszási idő 'elit junior' kategóriában > {lista.Where(V => V.VEletkorKategoria == "20-24").Average(V => V.UszasIdo.Minutes)} Perc");
+
+            Console.WriteLine("\n ---------- \n");
+
+            var NoiGyoztes = lista.Where(V => V.VNem == "Nő").OrderBy(V => V.VersenyzoOsszIdeje).First();
+
+            Console.WriteLine(NoiGyoztes.ToString());
+
+
+
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ triatlon/triatlon/Versenyzok.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace triatlon
+{
+    internal class Versenyzok
+    {
+        public string VNev { get; set; }
+        public int VSzulEv { get; set; }
+        public int VSzam {  get; set; }
+        public string VNem { get; set; }
+        public string VEletkorKategoria { get; set; }
+
+
+        public TimeSpan UszasIdo { get; set; }
+        public TimeSpan ElsoDepoIdo { get; set; }
+        public TimeSpan KerekparIdo { get; set; }
+        public TimeSpan MasodikDepo {  get; set; }
+        public TimeSpan FutasIdo { get; set; }
+
+        public TimeSpan VersenyzoOsszIdeje => UszasIdo + ElsoDepoIdo + KerekparIdo + MasodikDepo + FutasIdo;
+        public double OsszUszasIdo => UszasIdo.TotalHours + UszasIdo.TotalMinutes + UszasIdo.TotalSeconds;
+
+
+        public override string ToString()
+        {
+
+            return 
+                $"" +
+                $"Név: {VNev} \n" +
+                $"Születési év: {VSzulEv} \n" +
+                $"Rajtszám: {VSzam} \n" +
+                $"Neme: {VNem} \n" +
+                $"Életkor kategória: {VEletkorKategoria} \n" +
+                $"\n" +
+                $"Úszás ideje: {UszasIdo} \n" +
+                $"Első depóban töltött ideje: {ElsoDepoIdo} \n" +
+                $"Kerékpár ideje: {KerekparIdo} \n" +
+                $"Második depóban töltött ideje: {MasodikDepo} \n" +
+                $"Futás ideje: {FutasIdo} \n" +
+                $"Összideje (Úszás - Depó - Kerékpár - Depó - Futás): {VersenyzoOsszIdeje}";
+        }
+
+        public Versenyzok(string sor)
+        {
+            string[] darabok = sor.Split(';');
+
+            VNev = darabok[0];
+            VSzulEv = int.Parse(darabok[1]);
+            VSzam = int.Parse(darabok[2]);
+            if (darabok[3] == "n")
+            {
+                VNem = "Nő";
+            }
+            else
+            {
+                VNem = "Férfi";
+            }
+            VEletkorKategoria = darabok[4];
+
+            UszasIdo = TimeSpan.ParseExact(darabok[5], "hh\\:mm\\:ss", null);
+            ElsoDepoIdo = TimeSpan.ParseExact(darabok[6], "hh\\:mm\\:ss", null);
+            KerekparIdo = TimeSpan.ParseExact(darabok[7], "hh\\:mm\\:ss", null);
+            MasodikDepo = TimeSpan.ParseExact(darabok[8], "hh\\:mm\\:ss", null);
+            FutasIdo = TimeSpan.ParseExact(darabok[9], "hh\\:mm\\:ss", null);
+        }
+    }
+}
